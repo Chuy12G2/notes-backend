d42b70687214660db3723dd47dc721d36d4541f5
Jesus <chuygs12@gmail.com>
2023-01-27 16:16:43 -0600

    refactor it to follow best practices

---
+++ mongo.js
@@ -0,0 +1,47 @@
+// const mongoose = require('mongoose')
+// mongoose.set('strictQuery', true)
+
+// if (process.argv.length < 3) {
+//   console.log('Please provide the password as an argument: node mongo.js <password>')
+//   process.exit(1)
+// }
+
+// const password = process.argv[2]
+
+// const url = `mongodb+srv://app-notes-full:${password}@cluster0.zsat0r2.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   date: Date,
+//   important: Boolean,
+// })
+
+// const Note = mongoose.model('Note', noteSchema)
+
+// mongoose
+//   .connect(url)
+//   // .then((result) => {
+//   //   Note.find({}).then(result => {
+//   //     result.forEach(note => {
+//   //       console.log(note)
+//   //     })
+//   //     mongoose.connection.close()
+//   //   })
+//   // })
+//   .then((result) => {
+//     console.log('connected')
+
+//     const note = new Note({
+//       content: 'JS is weird',
+//       date: new Date(),
+//       important: true,
+//     })
+
+//     return note.save()
+//   })
+//   .then(() => {
+//     console.log('note saved!')
+//     return mongoose.connection.close()
+//   })
+//   .catch((err) => console.log(err))
+
